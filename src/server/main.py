8c2467a446c981cd85e1e2d22c8f0cf9b94e5af2
alexgiavaras <alex.giavaras@trilodocs.com>
2026-08-16 11:11:31 +0100

    feat: add server implementation

---
+++ src/server/main.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+BASE_DIR = Path(__file__).resolve().parent
+
+# Application instance
+app = FastAPI()
+
+# HTML template for the views
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
+# Where to fetch the static files for the views
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
